f9fed4d2b361a56258b5a095b1fd2b861e8f89ea
beitlab <gjejen@unal.edu.co>
2018-12-28 22:37:09 -0500

    ultimos arreglos

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-const config = require('./config')
-
-app.use(express.static('public'));
-//app.use(express.static('images'));
-
-app.listen(config.port);
